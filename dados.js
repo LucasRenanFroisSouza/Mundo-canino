e25daf5d6527a1db5fe0be9f3a63cfab31da5a58
lucas444x <lucasxbox708@gmail.com>
2024-09-06 19:15:03 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo : "Pastor alemão",
+        descrição : "O Pastor Alemão é uma raça de cão conhecida por sua inteligência, lealdade e versatilidade. Originalmente criado para o trabalho de pastoreio, essa raça se destacou em diversas áreas, como serviço policial, militar e como cão-guia. Sua aparência imponente, com pelagem densa e expressão alerta, aliada a um temperamento equilibrado, faz do Pastor Alemão um companheiro fiel e protetor para muitas famílias. Sua energia e necessidade de atividades físicas regulares exigem um dono ativo e disposto a proporcionar estímulos mentais e físicos para garantir seu bem-estar.",
+        link : "https://www.petz.com.br/cachorro/racas/pastor-alemao/",
+        tags: "pastor alemao"
+    },
+    {
+        titulo : "Rottweiler",
+        descrição : "O Rottweiler é uma raça de cão forte e poderosa, conhecida por sua lealdade e protetividade. Originalmente criado para o trabalho de guarda e transporte de cargas, o Rottweiler é um cão de família dedicado e protetor. Sua aparência imponente, com pelagem curta e densa, e sua natureza confiante exigem um dono experiente e firme, capaz de fornecer a socialização e o treinamento adequados.",
+        link : "https://www.petz.com.br/cachorro/racas/rottweiler/",
+        tags: ""
+    },
+    {
+        titulo : "Pitibull",
+        descrição : "O Pitbull, apesar de sua má fama, é uma raça de cão inteligente, enérgica e afetuosa. Sua força e determinação o tornam um excelente cão de trabalho, mas também um companheiro leal e divertido. É importante ressaltar que a socialização e o treinamento adequados são essenciais para o bem-estar do Pitbull e para que ele se desenvolva de forma equilibrada.",
+        link : "https://www.petz.com.br/cachorro/racas/pitbull/",
+        tags: ""
+    }
+];
