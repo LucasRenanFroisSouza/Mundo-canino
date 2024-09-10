0d7753b507262830f666621e4c9e2cf3cd818f66
lucas444x <lucasxbox708@gmail.com>
2024-09-09 21:42:45 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -12,9 +12,274 @@ let dados = [
         tags: ""
     },
     {
-        titulo : "Pitibull",
+        titulo : "Pitbull",
         descrição : "O Pitbull, apesar de sua má fama, é uma raça de cão inteligente, enérgica e afetuosa. Sua força e determinação o tornam um excelente cão de trabalho, mas também um companheiro leal e divertido. É importante ressaltar que a socialização e o treinamento adequados são essenciais para o bem-estar do Pitbull e para que ele se desenvolva de forma equilibrada.",
         link : "https://www.petz.com.br/cachorro/racas/pitbull/",
         tags: ""
-    }
+    },
+{
+    titulo : "Golden Retriever",
+    descrição : "Cão de porte médio-grande, conhecido por sua pelagem dourada e personalidade amigável. São muito inteligentes e fáceis de treinar, além de serem ótimos cães de família.",
+    link : "https://www.petz.com.br/cachorro/racas/golden-retriever/",
+    tags: ""
+},
+{
+    titulo : "Labrador Retriever",
+    descrição : "Outra raça popular, o Labrador é conhecido por sua natureza amável e energia. Eles são ótimos cães para famílias e são frequentemente usados como cães de serviço.",
+    link : "https://www.petz.com.br/cachorro/racas/labrador/",
+    tags: ""
+},
+{
+    titulo : "Poodle",
+    descrição : "O Poodle é conhecido por sua inteligência e facilidade de treinamento. Eles vêm em três tamanhos (toy, miniatura e padrão) e são hipoalergênicos, tornando-os uma escolha popular para pessoas com alergias.",
+    link : "https://www.petz.com.br/cachorro/racas/poodle/",
+    tags: ""
+},
+{
+    titulo : "Bulldog Francês",
+    descrição : "O Bulldog Francês é uma raça de pequeno porte com uma personalidade afetuosa e brincalhona. Eles são conhecidos por suas orelhas grandes e expressivas e por serem companheiros leais.",
+    link : "https://www.petz.com.br/cachorro/racas/buldogue-frances/",
+    tags: ""
+},
+{
+    titulo: "Beagle",
+    descrição: "O Beagle é uma raça de porte médio, conhecida por seu excelente faro e energia. Eles são cães curiosos e amigáveis, ideais para famílias ativas.",
+    link: "https://www.petz.com.br/cachorro/racas/beagle/",
+    tags: "médio, faro, energético, curioso, amigável"
+},
+{
+    titulo: "Chihuahua",
+    descrição: "O Chihuahua é uma das menores raças de cães, mas o que lhes falta em tamanho, eles compensam em personalidade. Eles são corajosos e formam um forte vínculo com seus donos.",
+    link: "https://www.petz.com.br/cachorro/racas/chihuahua/",
+    tags: "pequeno, corajoso, forte vínculo, dono"
+},
+{
+    titulo: "Shih Tzu",
+    descrição: "O Shih Tzu é uma raça de pequeno porte, conhecida por sua pelagem longa e personalidade carinhosa. Eles são excelentes cães de companhia e se adaptam bem a ambientes internos.",
+    link: "https://www.petz.com.br/cachorro/racas/shih-tzu/",
+    tags: "pequeno, pelagem longa, carinhoso, companhia, ambientes internos"
+},
+
+{
+    titulo: "Yorkshire Terrier",
+    descrição: "O Yorkshire Terrier é uma raça de pequeno porte com uma personalidade vivaz e destemida. Eles têm uma pelagem sedosa e são conhecidos por serem ótimos cães de apartamento.",
+    link: "https://www.petz.com.br/cachorro/racas/yorkshire-terrier/",
+    tags: "pequeno, vivaz, destemido, pelagem sedosa, cão de apartamento"
+},
+{
+    titulo: "Dachshund",
+    descrição: "O Dachshund, também conhecido como salsichinha, é uma raça de pequeno porte com corpo alongado e pernas curtas. Eles são corajosos, alertas e ótimos caçadores de tocas.",
+    link: "https://www.petz.com.br/cachorro/racas/dachshund/",
+    tags: "pequeno, corpo alongado, corajoso, alerta, caçador"
+},
+{
+    titulo: "Schnauzer",
+    descrição: "O Schnauzer é uma raça que vem em três tamanhos: miniatura, standard e gigante. Eles são conhecidos por sua aparência distinta e natureza leal.",
+    link: "https://www.petz.com.br/cachorro/racas/schnauzer/",
+    tags: "miniatura, standard, gigante, aparência distinta, leal"
+},
+{
+    titulo: "Pug",
+    descrição: "O Pug é uma raça de pequeno porte conhecida por seu focinho achatado e expressão carismática. Eles são brincalhões, amigáveis e ótimos companheiros.",
+    link: "https://www.petz.com.br/cachorro/racas/pug/",
+    tags: "pequeno, focinho achatado, carismático, brincalhão, amigável"
+},
+{
+    titulo: "Boxer",
+    descrição: "O Boxer é uma raça de porte médio a grande, conhecida por sua energia e lealdade. Eles são cães de guarda excelentes e também são muito afetuosos com a família.",
+    link: "https://www.petz.com.br/cachorro/racas/boxer/",
+    tags: "médio, grande, enérgico, leal, cão de guarda, afetuoso"
+},
+{
+    titulo: "Cocker Spaniel",
+    descrição: "O Cocker Spaniel é uma raça de porte médio, conhecida por suas longas orelhas e pelagem macia. Eles são cães alegres, amigáveis e adoram atividades ao ar livre.",
+    link: "https://www.petz.com.br/cachorro/racas/cocker-spaniel/",
+    tags: "médio, orelhas longas, pelagem macia, alegre, amigável, atividades ao ar livre"
+},
+{
+    titulo: "Border Collie",
+    descrição: "O Border Collie é uma raça de porte médio, altamente inteligente e conhecida por sua habilidade em pastoreio. Eles são energéticos e precisam de muito exercício e estímulo mental.",
+    link: "https://www.petz.com.br/cachorro/racas/border-collie/",
+    tags: "médio, inteligente, pastoreio, energético, exercício, estímulo mental"
+},
+{
+    titulo: "Maltês",
+    descrição: "O Maltês é uma raça de pequeno porte com uma pelagem branca e longa. Eles são amigáveis, elegantes e ótimos cães de companhia, especialmente em ambientes internos.",
+    link: "https://www.petz.com.br/cachorro/racas/maltes/",
+    tags: "pequeno, pelagem branca, longa, amigável, elegante, companhia"
+},
+{
+    titulo: "Husky Siberiano",
+    descrição: "O Husky Siberiano é uma raça de porte médio, conhecida por sua resistência e habilidade para puxar trenós. Eles têm uma aparência lupina e são muito ativos.",
+    link: "https://www.petz.com.br/cachorro/racas/husky-siberiano/",
+    tags: "médio, resistente, puxar trenós, aparência lupina, ativo"
+},
+{
+    titulo: "Doberman",
+    descrição: "O Doberman é uma raça de porte grande, conhecida por sua elegância, lealdade e habilidades como cão de guarda. Eles são inteligentes e protetores.",
+    link: "https://www.petz.com.br/cachorro/racas/doberman/",
+    tags: "grande, elegante, leal, cão de guarda, inteligente, protetor"
+},
+{
+    titulo: "Akita",
+    descrição: "O Akita é uma raça de porte grande, originária do Japão, conhecida por sua dignidade, coragem e lealdade. Eles são cães independentes e protetores.",
+    link: "https://www.petz.com.br/cachorro/racas/akita/",
+    tags: "grande, digno, corajoso, leal, independente, protetor"
+},
+{
+    titulo: "Basset Hound",
+    descrição: "O Basset Hound é uma raça de porte médio, conhecida por suas orelhas longas e excelente olfato. Eles são cães calmos, amigáveis e ótimos para caçar.",
+    link: "https://www.petz.com.br/cachorro/racas/basset-hound/",
+    tags: "médio, orelhas longas, excelente olfato, calmo, amigável, caçador"
+},
+{
+    titulo: "Lhasa Apso",
+    descrição: "O Lhasa Apso é uma raça de pequeno porte, conhecida por sua pelagem longa e sua natureza vigilante. Eles são cães independentes e foram originalmente criados para proteger mosteiros tibetanos.",
+    link: "https://www.petz.com.br/cachorro/racas/lhasa-apso/",
+    tags: "pequeno, pelagem longa, vigilante, independente, protetor"
+},
+{
+    titulo: "Weimaraner",
+    descrição: "O Weimaraner é uma raça de porte grande, conhecida por sua pelagem cinza e habilidades como cão de caça. Eles são cães enérgicos, leais e inteligentes.",
+    link: "https://www.petz.com.br/cachorro/racas/weimaraner/",
+    tags: "grande, pelagem cinza, cão de caça, enérgico, leal, inteligente"
+},
+{
+    titulo: "Samoyeda",
+    descrição: "O Samoyeda é uma raça de porte médio, conhecida por sua pelagem branca e fofa e seu sorriso característico. Eles são amigáveis, afetuosos e ótimos companheiros para climas frios.",
+    link: "https://www.petz.com.br/cachorro/racas/samoyeda/",
+    tags: "médio, pelagem branca, fofa, sorriso, amigável, afetuoso, clima frio"
+},
+{
+    titulo: "Bull Terrier",
+    descrição: "O Bull Terrier é uma raça de porte médio, conhecida por sua cabeça oval e personalidade energética. Eles são corajosos, leais e muito apegados aos seus donos.",
+    link: "https://www.petz.com.br/cachorro/racas/bull-terrier/",
+    tags: "médio, cabeça oval, energético, corajoso, leal, apegado"
+},
+{
+    titulo: "Boston Terrier",
+    descrição: "O Boston Terrier é uma raça de pequeno porte, conhecida por seu comportamento alegre e aparência elegante com seu manto preto e branco. Eles são ótimos cães de companhia.",
+    link: "https://www.petz.com.br/cachorro/racas/boston-terrier/",
+    tags: "pequeno, alegre, elegante, manto preto e branco, companhia"
+},
+{
+    titulo: "Cavalier King Charles Spaniel",
+    descrição: "O Cavalier King Charles Spaniel é uma raça de pequeno porte, conhecida por seu comportamento afetuoso e aparência graciosa. Eles são cães carinhosos e muito sociáveis.",
+    link: "https://www.petz.com.br/cachorro/racas/cavalier-king-charles-spaniel/",
+    tags: "pequeno, afetuoso, gracioso, carinhoso, sociável"
+},
+{
+    titulo: "Papillon",
+    descrição: "O Papillon é uma raça de pequeno porte, conhecida por suas orelhas grandes em forma de borboleta. Eles são inteligentes, ágeis e muito brincalhões.",
+    link: "https://www.petz.com.br/cachorro/racas/papillon/",
+    tags: "pequeno, orelhas grandes, borboleta, inteligente, ágil, brincalhão"
+},
+{
+    titulo: "Bichon Frisé",
+    descrição: "O Bichon Frisé é uma raça de pequeno porte, conhecida por sua pelagem branca e encaracolada. Eles são alegres, brincalhões e muito sociáveis.",
+    link: "https://www.petz.com.br/cachorro/racas/bichon-frise/",
+    tags: "pequeno, pelagem branca, encaracolada, alegre, brincalhão, sociável"
+},
+{
+    titulo: "Collie",
+    descrição: "O Collie é uma raça de porte médio a grande, conhecida por sua elegância e inteligência. Eles são muito leais e ótimos cães de pastoreio.",
+    link: "https://www.petz.com.br/cachorro/racas/collie/",
+    tags: "médio, grande, elegante, inteligente, leal, pastoreio"
+},
+{
+    titulo: "Pequinês",
+    descrição: "O Pequinês é uma raça de pequeno porte, conhecida por sua aparência majestosa e personalidade corajosa. Eles são independentes e foram originalmente criados como cães de companhia para a realeza chinesa.",
+    link: "https://www.petz.com.br/cachorro/racas/pequines/",
+    tags: "pequeno, majestoso, corajoso, independente, companhia, realeza chinesa"
+},
+{
+    titulo: "Dálmata",
+    descrição: "O Dálmata é uma raça de porte médio a grande, conhecida por sua pelagem branca com manchas pretas ou marrons. Eles são cães enérgicos, amigáveis e excelentes companheiros para atividades ao ar livre.",
+    link: "https://www.petz.com.br/cachorro/racas/dalmata/",
+    tags: "médio, grande, pelagem branca, manchas pretas, manchas marrons, enérgico, amigável, atividades ao ar livre"
+},
+{
+    titulo: "Chow Chow",
+    descrição: "O Chow Chow é uma raça de porte médio a grande, conhecida por sua língua azul-escura e pelagem espessa. Eles são cães independentes e leais, com uma personalidade reservada.",
+    link: "https://www.petz.com.br/cachorro/racas/chow-chow/",
+    tags: "médio, grande, língua azul-escura, pelagem espessa, independente, leal, reservado"
+},
+{
+    titulo: "Shar Pei",
+    descrição: "O Shar Pei é uma raça de porte médio, conhecida por sua pele enrugada e personalidade calma. Eles são cães leais e protetores, ideais como cães de guarda.",
+    link: "https://www.petz.com.br/cachorro/racas/shar-pei/",
+    tags: "médio, pele enrugada, calmo, leal, protetor, cão de guarda"
+},
+{
+    titulo: "Galgo",
+    descrição: "O Galgo, ou Greyhound, é uma raça de porte grande, conhecida por sua velocidade e elegância. Eles são cães dóceis e amigáveis, mas também excelentes corredores.",
+    link: "https://www.petz.com.br/cachorro/racas/galgo/",
+    tags: "grande, rápido, elegante, dócil, amigável, corredor"
+},
+{
+    titulo: "Whippet",
+    descrição: "O Whippet é uma raça de porte médio, conhecida por sua velocidade e agilidade. Eles são cães elegantes, tranquilos e ótimos para viver em ambientes internos.",
+    link: "https://www.petz.com.br/cachorro/racas/whippet/",
+    tags: "médio, rápido, ágil, elegante, tranquilo, ambiente interno"
+},
+{
+    titulo: "Springer Spaniel Inglês",
+    descrição: "O Springer Spaniel Inglês é uma raça de porte médio, conhecida por sua energia e habilidade como cão de caça. Eles são amigáveis, brincalhões e ótimos para famílias ativas.",
+    link: "https://www.petz.com.br/cachorro/racas/springer-spaniel-ingles/",
+    tags: "médio, energético, cão de caça, amigável, brincalhão, família ativa"
+},
+{
+    titulo: "Setter Irlandês",
+    descrição: "O Setter Irlandês é uma raça de porte grande, conhecida por sua pelagem vermelha e natureza amigável. Eles são cães energéticos, ideais para famílias que gostam de atividades ao ar livre.",
+    link: "https://www.petz.com.br/cachorro/racas/setter-irlandes/",
+    tags: "grande, pelagem vermelha, amigável, energético, atividades ao ar livre"
+},
+{
+    titulo: "West Highland White Terrier",
+    descrição: "O West Highland White Terrier, também conhecido como Westie, é uma raça de pequeno porte, conhecida por sua pelagem branca e personalidade destemida. Eles são cães leais, amigáveis e ótimos para ambientes internos.",
+    link: "https://www.petz.com.br/cachorro/racas/west-highland-white-terrier/",
+    tags: "pequeno, pelagem branca, destemido, leal, amigável, ambiente interno"
+},
+{
+    titulo: "Spitz Alemão",
+    descrição: "O Spitz Alemão é uma raça de pequeno porte, conhecida por sua pelagem fofa e personalidade alerta. Eles são cães leais, inteligentes e ótimos companheiros para a família.",
+    link: "https://www.petz.com.br/cachorro/racas/spitz-alemao/",
+    tags: "pequeno, pelagem fofa, alerta, leal, inteligente, companheiro"
+},
+{
+    titulo: "Pinscher",
+    descrição: "O Pinscher é uma raça de pequeno porte, conhecida por sua energia e personalidade corajosa. Eles são cães leais e vigilantes, ótimos para viver em ambientes internos.",
+    link: "https://www.petz.com.br/cachorro/racas/pinscher/",
+    tags: "pequeno, energético, corajoso, leal, vigilante, ambiente interno"
+},
+{
+    titulo: "Dachshund",
+    descrição: "O Dachshund, também conhecido como salsichinha, é uma raça de pequeno porte, conhecida por seu corpo longo e pernas curtas. Eles são cães corajosos, leais e ótimos caçadores.",
+    link: "https://www.petz.com.br/cachorro/racas/dachshund/",
+    tags: "pequeno, corpo longo, pernas curtas, corajoso, leal, caçador"
+},
+{
+    titulo: "Corgi",
+    descrição: "O Corgi é uma raça de porte pequeno a médio, conhecida por suas pernas curtas e natureza amigável. Eles são cães inteligentes, leais e ótimos companheiros para famílias.",
+    link: "https://www.petz.com.br/cachorro/racas/corgi/",
+    tags: "pequeno, médio, pernas curtas, amigável, inteligente, leal, companheiro"
+},
+{
+    titulo: "Chihuahua",
+    descrição: "O Chihuahua é uma raça de pequeno porte, conhecida por seu tamanho diminuto e personalidade corajosa. Eles são cães leais, vigilantes e ótimos para viver em ambientes internos.",
+    link: "https://www.petz.com.br/cachorro/racas/chihuahua/",
+    tags: "pequeno, diminuto, corajoso, leal, vigilante, ambiente interno"
+},
+{
+    titulo: "Cane Corso",
+    descrição: "O Cane Corso é uma raça de porte grande, conhecida por sua força e personalidade protetora. Eles são cães leais, corajosos e ótimos para a guarda de propriedades.",
+    link: "https://www.petz.com.br/cachorro/racas/cane-corso/",
+    tags: "grande, forte, protetor, leal, corajoso, guarda"
+},
+{
+titulo: "Fila Brasileiro",
+descrição: "O Fila Brasileiro é uma raça de porte grande, conhecida por sua força e instinto protetor. Eles são cães leais, corajosos e ótimos para a guarda de propriedades.",
+link: "https://www.petz.com.br/cachorro/racas/fila-brasileiro/",
+tags: "grande, forte, protetor, leal, corajoso, guarda"
+}
 ];
